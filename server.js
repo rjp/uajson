37f6f4c8603dcb28ce968fc54ad3a44632509503
rjp <zimpenfish@gmail.com>
2011-05-18 18:51:13 +0100

    - CHECKPOINT - clean up the list of modules we load - remove all the old cruft from uanotify - steal map() from UA3 - spawn_bot launches an event looped based bot instead of a real process - TODO work out all the scenarios for birth/death/alive and document - uaclient no longer emits events for replies, moved to callbacks instead - implemented: /message/read, /message/:id, /folders/unread, /folders/subscribed, /folders, /system, /folder/:name - TODO implement /user et al - reaper() runs every 15 seconds to clear out old connections

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 var jade = require('jade');
 var sys = require('sys');
 var uaclient = require('uaclient');
-var redisFactory = require('redis-node');
 var connect = require('./connect/lib/connect/index');
-var spawn = require('child_process').spawn;
 var fs = require('fs');
-var Log = require('log'), log = new Log(Log.WARNING);
+var Log = require('log'), log = new Log(Log.INFO);
 require('./wordwrap.js');
-var uajson = require('./ua.js'); // functions for converting EDF responses to JSON
-
+// functions for converting EDF responses to JSON
+var uajson = require('./ua.js');
 var uajson = new uajson.uajson;
 
 var g_req;
 var g_res;
+var g_folders = {};
 
-function cache_folders(child, ting) {
+var SESSION_TIMEOUT = 300 * 1000;
+
+// use a hashed mix for the session key to avoid hijacking
+var crypto = require('crypto');
+function session_key(ingredients) {
+    var unhashed_key = ingredients.join('!');
+    return crypto.createHash('md5').update(unhashed_key).digest("hex");
 }
+
+// how do we block without blocking?
+function folder_info(foldername, callback) {
+    callback({folder_id: g_folders[foldername]});
+}
+
+function cache_folders(ting, success) {
+//   log.warning(sys.inspect(thash));
+    log.warning("cache_folders");
+    success();
+}
+
 function announce_message_add(child, ting) {
     child.flatten(ting);
     sys.puts("AMA for "+child.username);
@@ -25,15 +42,10 @@ var api_keys = require(process.env.HOME + '/.apikeys.js');
 
 var TooQuickSpawn = 5 * 60 * 1000; // 5 minutes
 
-var redis = redisFactory.createClient();
 var h = process.cwd();
 
 var config_json = fs.readFileSync(process.argv[2], 'utf8');
 var config = JSON.parse(config_json);
-
-if (config.frequency == undefined) {
-    config.frequency = {"7200": "Two hours", "14400":"Four hours"};
-}
 
 var server = connect.createServer(
     connect.logger({ format: ':method :url' }),
@@ -49,59 +61,9 @@ log.info('Connect server listening on port '+config.port);
 // holds information about our child bots
 var ua_sessions = {};
 
-// temporary fix for the broken packet handling
-// inspired by a smart cheese
-function serial_mget (redis, list, final_callback) {
-    var ilist = new Array;
-    var xlist = new Array;
-    var lsize = list.length;
-    for(var i=0; i++; i<lsize) { xlist[i] = 1; }
-
-    var mid_callback = function(i, lsize) {
-        var q = i;
-        return function(err, val){
-	        if (err) final_callback(err, undefined);
-	        ilist.push(val);
-            xlist[q] = ilist.length - 1; // index of key in the list
-	        if (ilist.length == lsize) {
-                var nlist = new Array;
-                for(var i in xlist) {
-                    nlist[i] = ilist[xlist[i]]; // transpose keys
-                }
-	            final_callback(undefined, nlist);
-		    }
-        };
-    };
-    // perform all the lookups
-    // TODO does this need to be in a transaction?
-    for(var i in list) {
-        redis.get(list[i], mid_callback(i, lsize));
-    }
-}
-
 function authenticate(user, pass, success, failure) {
     log.info('authinfo is '+user+':'+pass+"; "+failure);
     spawn_bot(user, pass, 'http_auth', success, failure);
-}
-
-function output_message(req, res, x, t) {
-    var template = t;
-    if (t == undefined) { template = 'message.html'; } // default to the full page
-    var a = JSON.parse(x);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    log.info("TO="+a.m_toname);
-    var to = a.m_toname == undefined ? undefined : a.m_toname;
-    d = new Date(a.m_date * 1000);
-    b = d.toLocaleString();
-    c = b.substr(16,5) +', '+ b.substr(0,3) +' '+ b.substr(8,2) +'/' + ('00'+(1+d.getMonth())).substr(-2);
-    summary = a.foldername+'/'+a.message+' ('+a.m_msgpos+'/'+a.nummsgs+') at '+c;
-    jade.renderFile('message.html', { locals: {
-        summary: summary, from: a.m_fromname, to: to,
-        subject: a.m_subject, body: a.m_text,
-        wrapped: String.wordwrap(a.m_text)
-        }}, function(err, html){
-        res.end(html);
-    });
 }
 
 function buffer_to_strings(x) {
@@ -113,39 +75,6 @@ function buffer_to_strings(x) {
     return x;
 }
 
-function output_links(req, res, x) {
-    // convert our array of buffers to the JSON strings
-    buffer_to_strings(x);
-
-    var posts = [];
-    for(var i in x) {
-        try {
-        m = JSON.parse(x[i]);
-        } catch(e) {
-            log.critical(sys.inspect(x[i]));
-            process.exit(42);
-        }
-	    d = new Date(m.m_date * 1000);
-	    b = d.toLocaleString();
-	    c = b.substr(16,5) +', '+ b.substr(0,3) +' '+ b.substr(8,2) +'/' + ('00'+(1+d.getMonth())).substr(-2);
-        m.nicedate = c;
-        m.flat_text = m.m_text.replace(/\n/g,' &sect; ');
-        if (m.flat_text.length > 60) {
-            m.flat_text = m.flat_text.substr(0,59) + '...';
-        }
-        m.to = (m.m_toname == undefined) ? '&nbsp;' : m.m_toname;
-        m.wrapped = String.wordwrap(m.text).replace(/\n\s*\n/g, "<br/><br/>");
-        posts.push(m);
-    }
-    log.info(sys.inspect(posts[0]));
-
-    jade.renderFile('list.html', { locals: { posts: posts } },
-        function(err, html){ 
-        log.warning(err);
-        res.end(html); 
-    });
-}
-
 function debuffer_hash(h) {
     for(var i in h) {
         if (typeof h[i] === 'string') {
@@ -153,6 +82,30 @@ function debuffer_hash(h) {
         }
     }
 }
+
+// perform asynchronous callbacks for each item in a list and then
+// pass the new list off to a final callback
+function map(list, each_callback, final_callback) {
+    // shortcut any processing if we've got an empty list
+    if (list === undefined || list === null || list.length === 0) {
+        final_callback(undefined, []);
+        return;
+    }
+    var ilist = [];
+    var lsize = list.length;
+    var mid_callback = function(err, val){
+        if (err) { final_callback(err, undefined); }
+        ilist.push(val);
+        if (ilist.length == lsize) {
+            final_callback(undefined, ilist);
+	    }
+    };
+    for(var i in list) {
+        if (list.hasOwnProperty(i)) {
+            each_callback(list[i], i, mid_callback);
+        }
+    }
+};
 
 // reason is 'boot', 'settings' or 'interval'
 function spawn_bot(user, pass, reason, success, failure) {
@@ -164,9 +117,18 @@ function spawn_bot(user, pass, reason, success, failure) {
         should_spawn = true;
     }
 
-    if (ua_sessions[user] != undefined) { // we have a flag
-        log.info("SB: "+user+": alive");
-        ua_sessions[user].last = now; // record the last alive time
+    // TODO decide if we need port in this mix
+    var my_key = user;
+
+    if (ua_sessions[my_key] != undefined) { // we have a flag
+        if (ua_sessions[my_key].pass == pass) {
+	        log.info("SB: "+user+": alive");
+	        ua_sessions[my_key].last = now; // record the last alive time
+        } else {
+            log.warning("SB: trying to reuse session with incorrect password");
+            failure();
+            return;
+        }
     } else {
         log.info("SB: "+user+": not alive");
         should_spawn = true;
@@ -202,26 +164,23 @@ function spawn_bot(user, pass, reason, success, failure) {
         child.shadow = 256;
         child.caching = false;
 
-//        child.addListener("folders", cache_folders);
         child.addListener("announce_message_add", function(a){
             announce_message_add(child, a);
         });
 
-        child.addListener("reply_folder_list", function(a){
-            var json = uajson.reply_folder_list(a, child);
-            g_res.writeHead(200, {'Content-Type':'text/html'});
-            jade.renderFile('folders.html', { locals: {"a":json, "b":sys.inspect(a)} },
-                function(err, html){
-                    g_res.end(html);
-                });
+        child.addListener("reply_message_list", function(a){
+            var json = uajson.reply_message_list(a, child);
+            g_res.writeHead(200, {'Content-Type':'application/json'});
+            g_res.end(JSON.stringify(json));
         });
         child.addListener("reply_user_login", function(a){
-            log.info("-> LOGGED ON <-, call the HTTP success bits!");
+            log.info("-> LOGGED ON <-, pending folders");
+            child.req_folder_list(child);
             success();
         });
         child.addListener("reply_user_login_invalid", function(a){
             log.warning("Authentication failure, returning 401");
-            ua_sessions[user] = undefined;
+            delete ua_sessions[user];
             failure();
         });
 
@@ -229,67 +188,143 @@ function spawn_bot(user, pass, reason, success, failure) {
         child.addListener('finished', function(data, code){
             log.warning("finished "+code);
             log.warning(sys.inspect(data));
-            ua_sessions[user] = undefined;
+            delete ua_sessions[my_key];
         });
 
-        child.addListener('folders', function(){
-                log.info("<folders> should be cached or ignored");
+        child.addListener('folder-cache', function(a, ah){
+            // should we clear the cache here?
+            g_folders = {};
+            for(var i in ah) {
+                if (ah.hasOwnProperty(i)) {
+                    var fname = i.toLowerCase();
+                    var fid = ah[i];
+                    g_folders[fname] = fid;
+                }
+            }
         });
 
-        ua_sessions[user] = { session: child, last: now };
+        ua_sessions[my_key] = { session: child, last: now, pass: pass };
         child.connect(user, pass, config['ua_host'], config['ua_port']);
 }
-
-function spawn_bots(reason) {
-    sys.puts("Nothing here, bots are spawned on demand.");
-}
-
-spawn_bots('boot');
-setInterval(function(){spawn_bots('respawn')}, 60 * 1000);
 
 function get_user_info(auth, callback) {
     callback();
 }
 
 function app(app) {
-    app.get('/m/:id', function(req, res){
-        redis.get(req.params.id, function(err, x){
-            if (err) { throw(err); }
-            if (x != undefined) {
-                output_message(req, res, x);
-            } else {
-                res.writeHead(302, { Location: '/expired' });
-                res.end();
-            };
+    app.post('/message/read', function(req, res) {
+        var messages = JSON.parse(req.body.messages);
+        log.info(sys.inspect(messages));
+        var my_key = req.remoteUser;
+        var myself = ua_sessions[my_key].session;
+        map(messages, function(f, i, c) {
+            myself.request('message_mark_read', {"messageid":f}, c);
+        }, function(e, newlist) {
+            res.writeHead(200, {'Content-Type':'application/json'});
+            res.end(JSON.stringify({"ok":"ok"}));
         });
-        console.log('return message '+req.params.id)
     });
-    app.get('/l/:id', function(req, res){
-        // we're returning HTML, let's tell the browser that
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        redis.zrange('sorted:'+req.params.id, 0, -1, function(err, x){
-            if (err == undefined) {
-                if (x.length > 0) {
-                    serial_mget(redis, x, function(err, messages){
-                        if (err) throw(err);
-                        output_links(req, res, messages);
-                    });
+
+    app.get('/message/:id', function(req,res){
+        var my_key = req.remoteUser;
+        var myself = ua_sessions[my_key].session;
+        myself.request('message_list', {"messageid":parseInt(req.params.id, 10)}, function(t, a) {
+                if (t == 'message_list') { 
+                    log.info(sys.inspect(a));
+		            var json = uajson.reply_message_list(a, myself);
+                    res.writeHead(200, {'Content-Type':'application/json'});
+	                res.end(JSON.stringify(json[0])); // only one
                 } else {
-                    jade.renderFile('empty.html', {}, function(e, h){
-                        if (e) throw(e);
-                        res.end(h)
-                    });
+                    res.writeHead(404, {'Content-Type':'application/json'});
+	                res.end(JSON.stringify({"error":"no such message"}));
                 }
-            }
         });
-        console.log('return list '+req.params.id)
     });
-    app.get('/folders', function(req,res){
-        g_req = req;
-        g_res = res;
+
+    app.get('/folders/unread', function(req,res){
         log.warning("Auth OK, requesting a folder list");
-        res.writeHead(200, {'Content-Type':'text/html'});
-        ua_sessions['rjp'].session.request('folder_list', {"searchtype":3});
+        // tricky!
+        var my_key = req.remoteUser;
+        var myself = ua_sessions[my_key].session;
+        myself.request('folder_list', {"searchtype":3}, function(t, a) {
+	            var raw_json = uajson.reply_folder_list(a, myself);
+                var json = [];
+                for (var i in raw_json) {
+                    if (raw_json.hasOwnProperty(i) &&
+                        raw_json[i].unread > 0) {
+                        json.push(raw_json[i]);
+                    }
+                }
+                res.writeHead(200, {'Content-Type':'application/json'});
+                res.end(JSON.stringify(json));
+        });
+    });
+
+    app.get('/folders/subscribed', function(req,res){
+        log.warning("Auth OK, requesting a folder list");
+        // tricky!
+        var my_key = req.remoteUser;
+        var myself = ua_sessions[my_key].session;
+        myself.request('folder_list', {"searchtype":3}, function(t, a) {
+	            var raw_json = uajson.reply_folder_list(a, myself);
+                var json = [];
+                for (var i in raw_json) {
+                    if (raw_json.hasOwnProperty(i) &&
+                        raw_json[i].subscribed) {
+                        json.push(raw_json[i]);
+                    }
+                }
+                res.writeHead(200, {'Content-Type':'application/json'});
+                res.end(JSON.stringify(json));
+        });
+    });
+
+    app.get('/folders', function(req,res){
+        log.warning("Auth OK, requesting a folder list");
+        // tricky!
+        var my_key = req.remoteUser;
+        var myself = ua_sessions[my_key].session;
+        myself.request('folder_list', {"searchtype":3}, function(t, a) {
+	            var json = uajson.reply_folder_list(a, myself);
+                res.writeHead(200, {'Content-Type':'application/json'});
+                res.end(JSON.stringify(json));
+        });
+    });
+
+    app.get('/system', function(req, res){
+        res.writeHead(200, {'Content-Type':'application/json'});
+        res.end(JSON.stringify({"banner":"No banner here"}));
+    });
+
+    app.get('/folder/:name', function(req,res){
+        log.warning("Auth OK, requesting a message list");
+        res.writeHead(200, {'Content-Type':'application/json'});
+        var folder = req.params.name.toLowerCase();
+        folder_info(folder, function(folderinfo){
+            // tricky!
+            var my_key = req.remoteUser;
+            var myself = ua_sessions[my_key].session;
+            myself.request('message_list', {"folderid":folderinfo.folder_id, "searchtype":1}, function(t, a) {
+	            var json = uajson.reply_message_list(a, myself);
+	            res.writeHead(200, {'Content-Type':'application/json'});
+	            res.end(JSON.stringify(json));
+            });
+        });
     });
 }
 
+function reaper() {
+    var now = new Date().getTime();
+    for(i in ua_sessions) {
+        if (ua_sessions.hasOwnProperty(i) &&
+            now - ua_sessions[i].last > SESSION_TIMEOUT) {
+            log.info("reaping session for "+i);
+            ua_sessions[i].session.stream.end();
+            log.info(sys.inspect(ua_sessions[i].session.stream));
+            delete ua_sessions[i];
+        }
+    }
+}
+
+// call the reaper every 15 seconds
+setInterval(reaper, 15000);
